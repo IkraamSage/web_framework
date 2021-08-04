76f010da56dff45a93622821aa0c5e9df097e7dc
Ikraam Sage <i.sage0007@gmail.com>
2021-08-04 10:17:14 +0200

    Initialize Git repository

--- app.py
+++ app.py
@@ -1,7 +1,8 @@
 import hmac
 import sqlite3
+import datetime
 
-from flask import Flask, request, jsonify
+from flask import Flask, request
 from flask_jwt import JWT, jwt_required, current_identity
 from flask_cors import CORS
 
@@ -14,7 +15,7 @@ class User(object):
 
 
 def fetch_users():
-    with sqlite3.connect('POS.db') as conn:
+    with sqlite3.connect('products.db') as conn:
         cursor = conn.cursor()
         cursor.execute("SELECT * FROM user")
         users = cursor.fetchall()
@@ -26,8 +27,11 @@ def fetch_users():
     return new_data
 
 
+users = fetch_users()
+
+
 def init_user_table():
-    conn = sqlite3.connect('POS.db')
+    conn = sqlite3.connect('products.db')
     print("Opened database successfully")
 
     conn.execute("CREATE TABLE IF NOT EXISTS user(user_id INTEGER PRIMARY KEY AUTOINCREMENT,"
@@ -39,18 +43,19 @@ def init_user_table():
     conn.close()
 
 
-def init_products_table():
-    with sqlite3.connect('POS.db') as conn:
-        conn.execute("CREATE TABLE IF NOT EXISTS products (id INTEGER PRIMARY KEY AUTOINCREMENT,"
+def init_product_table():
+    with sqlite3.connect('products.db') as conn:
+        conn.execute("CREATE TABLE IF NOT EXISTS product(id INTEGER PRIMARY KEY AUTOINCREMENT,"
                      "title TEXT NOT NULL,"
-                     "content TEXT NOT NULL,"
-                     "price TEXT NOT NULL)")
-    print("product table created successfully.")
+                     "description TEXT NOT NULL,"
+                     "price TEXT NOT NULL,"
+                     "category TEXT NOT NULL"
+                     "date_created TEXT NOT NULL)")
+    print("blog table created successfully.")
 
 
 init_user_table()
-init_products_table()
-users = fetch_users()
+init_product_table()
 
 username_table = {u.username: u for u in users}
 userid_table = {u.id: u for u in users}
@@ -75,28 +80,87 @@ CORS(app)
 jwt = JWT(app, authenticate, identity)
 
 
-@app.route('/registration/', methods=["POST"])
+@app.route('/protected')
+@jwt_required()
+def protected():
+    return '%s' % current_identity
+
+
+@app.route('/user-registration/', methods=["POST"])
 def user_registration():
     response = {}
 
     if request.method == "POST":
 
-        name = request.form['name']
+        first_name = request.form['first_name']
+        last_name = request.form['last_name']
         username = request.form['username']
         password = request.form['password']
 
-        with sqlite3.connect("POS.db") as conn:
+        with sqlite3.connect("products.db") as conn:
             cursor = conn.cursor()
             cursor.execute("INSERT INTO user("
-                           "name,"
+                           "first_name,"
+                           "last_name,"
                            "username,"
-                           "password) VALUES(?, ?, ?)", (name, username, password))
+                           "password) VALUES(?, ?, ?, ?)", (first_name, last_name, username, password))
             conn.commit()
             response["message"] = "success"
             response["status_code"] = 201
         return response
 
 
+@app.route('/add-product/', methods=["POST"])
+@jwt_required()
+def add_product():
+    response = {}
+
+    if request.method == "POST":
+        title = request.form['title']
+        description = request.form['description']
+        price = request.form['price']
+        category = request.form['category']
+        date_created = datetime.datetime.now()
+
+        with sqlite3.connect('products.db') as conn:
+            cursor = conn.cursor()
+            cursor.execute("INSERT INTO post("
+                           "title,"
+                           "description,"
+                           "price,"
+                           "category,"
+                           "date_created) VALUES(?, ?, ?, ?, ?)", (title, description, price, category, date_created))
+            conn.commit()
+            response["status_code"] = 201
+            response['description'] = "Product added successfully"
+        return response
+
+
+@app.route('/get-cart/', methods=["GET"])
+def get_cart():
+    response = {}
+    with sqlite3.connect("products.db") as conn:
+        cursor = conn.cursor()
+        cursor.execute("SELECT * FROM product")
+
+        products = cursor.fetchall()
+
+    response['status_code'] = 200
+    response['data'] = products
+    return response
+
+
+@app.route("/remove-product/<int:product_id>")
+@jwt_required()
+def remove_product(product_id):
+    response = {}
+    with sqlite3.connect("products.db") as conn:
+        cursor = conn.cursor()
+        cursor.execute("DELETE FROM product WHERE id=" + str(product_id))
+        conn.commit()
+        response['status_code'] = 200
+        response['message'] = "Product removed successfully."
+    return response
+
 if __name__ == "__main__":
-    app.debug = True
     app.run()
